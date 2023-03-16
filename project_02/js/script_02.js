672e7332302e5989f53d69111a53578c3b4292af
Ron-Watan <ronwatan@outlook.com>
2023-03-16 10:28:40 -0700

    Add Accordion

--- project_02/js/script_02.js
+++ project_02/js/script_02.js
@@ -54,3 +54,45 @@ const obsOption = {
 const triggerObserver = new IntersectionObserver(obsCallbackHeader, obsOption);
 
 triggerObserver.observe(trigger);
+
+
+
+
+/////////////////////////
+const temp = document.querySelectorAll('.containerInner')
+const slidebtn = document.querySelector('.partnerContainer__slide')
+const parentEl = document.querySelector('.partnerContainer')
+/*
+slidebtn.addEventListener('click', function () {
+  const heightContentP = document.querySelector('.partnerContainer__paragraph').scrollHeight
+
+  if (temp.style.height == 0) {
+    temp.style.height = `${heightContentP + 20}px`
+
+  } else {
+    temp.style.height = null
+
+  }
+
+  // temp.classList.toggle('activeSlide')
+})
+
+*/
+console.log(parentEl)
+parentEl.addEventListener('click', function (e) {
+  const target = e.target.closest('.partnerContainer__slide')
+  const heightContentP = (target.childNodes[3].childNodes[1].childNodes[1]).scrollHeight
+
+
+  if (target.childNodes[3].style.height == 0) {
+    temp.forEach(el => {
+      el.style.height = null
+    })
+    target.childNodes[3].style.height = `${heightContentP + 20}px`
+
+  } else {
+    target.childNodes[3].style.height = null
+
+  }
+
+})
